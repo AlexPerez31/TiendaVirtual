6cdbbfff9ffdced79a48429b1fec5b47616e0421
Alex Jordán Pérez Rojas <74392631+AlexPerez31@users.noreply.github.com>
2022-06-11 00:50:11 -0500

    Add files via upload

---
+++ Tienda/src/tienda/Productos.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tienda;
+
+/**
+ *
+ * @author Jordan y Freddy
+ */
+public class Productos {
+    
+    private String swich="Nintendo Switch JoY-Con  $1.049.900   CANTIDAD: ";
+    private String ps4="PlayStation 4 1 Tera  $1.099.000   CANTIDAD: ";
+    private String ones="Xbox One S 1 Tb  $1.199.000  CANTIDAD: ";
+    private String onex="Xbox One X 1 Tb  $1.785.400  CANTIDAD: ";
+    private String ds="Nintendo 3ds Xl  $599.900   CANTIDAD: ";
+    private String psv="Sony PS Vita  $754.900  CANTIDAD: ";
+    private String zelda="The Legend Of Zelda Breath The Wild   $135.900   CANTIDAD: ";
+    private String god="God Of War 4  $159.800   CANTIDAD: ";
+    private String red="Red Dead Redemptions 2 Xbox One  $365.990   CANTIDAD: ";
+
+    public String getSwich() {
+        return swich;
+    }
+
+    public void setSwich(String swich) {
+        this.swich = swich;
+    }
+
+    public String getPs4() {
+        return ps4;
+    }
+
+    public void setPs4(String ps4) {
+        this.ps4 = ps4;
+    }
+
+    public String getOnes() {
+        return ones;
+    }
+
+    public void setOnes(String ones) {
+        this.ones = ones;
+    }
+
+    public String getOnex() {
+        return onex;
+    }
+
+    public void setOnex(String onex) {
+        this.onex = onex;
+    }
+
+    public String getDs() {
+        return ds;
+    }
+
+    public void setDs(String ds) {
+        this.ds = ds;
+    }
+
+    public String getPsv() {
+        return psv;
+    }
+
+    public void setPsv(String psv) {
+        this.psv = psv;
+    }
+
+    public String getZelda() {
+        return zelda;
+    }
+
+    public void setZelda(String zelda) {
+        this.zelda = zelda;
+    }
+
+    public String getGod() {
+        return god;
+    }
+
+    public void setGod(String god) {
+        this.god = god;
+    }
+
+    public String getRed() {
+        return red;
+    }
+
+    public void setRed(String red) {
+        this.red = red;
+    }
+
+   
+}
